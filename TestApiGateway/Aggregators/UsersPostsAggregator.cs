114518120675db726500a0169d14c199aa4f9eeb
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-01-25 11:31:43 -0300

    initial

---
+++ TestApiGateway/Aggregators/UsersPostsAggregator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TestApiGateWay.Dtos;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+
+namespace TestApiGateWay.Aggregators
+{
+    public class UsersPostsAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            if (responses.Any(r => r.Items.Errors().Count > 0))
+            {
+                return new DownstreamResponse(null, HttpStatusCode.BadRequest, (List<Header>) null, null);
+            }
+        
+            var userResponseContent = await responses[0].Items.DownstreamResponse().Content.ReadAsStringAsync();
+            var postResponseContent = await responses[1].Items.DownstreamResponse().Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<List<User>>(userResponseContent);
+            var posts = JsonConvert.DeserializeObject<List<Post>>(postResponseContent);
+
+            foreach (var user in users)
+            {
+                var userPosts = posts.Where(p => p.UserId == user.Id).ToList();
+                user.Posts.AddRange(userPosts);
+            }
+
+            var postByUserString = JsonConvert.SerializeObject(users);
+
+            var stringContent = new StringContent(postByUserString)
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+        }
+    }
+}
---
+++ TokenGenerator/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TokenGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("token")]
+        public async Task<IActionResult> Token(Credentials credentials)
+        {
+            if (!IsAdmin(credentials) && !IsUser(credentials))
+            {
+                return Unauthorized();
+            }
+
+            var secretKey = "MySuperSecretKeyForOnlyTest";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var jwt = new JwtSecurityToken(
+                            claims: BuildClaims(credentials),
+                            expires: DateTime.Now.AddMinutes(15),
+                            signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512)
+            );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwt);
+            return Ok(token);
+        }
+
+        private Claim[] BuildClaims(Credentials credentials)
+        {
+            return new[]
+            {
+                new Claim("userType", IsAdmin(credentials) ? "admin" : "user")
+            };
+        }
+
+        private bool IsUser(Credentials credentials)
+        {
+            return credentials.UserName == "user" && credentials.Password == "user";
+        }
+
+        private bool IsAdmin(Credentials credentials)
+        {
+            return credentials.UserName == "admin" && credentials.Password == "admin";
+        }
+    }
+
+    public class Credentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
